8dac70230b85dee33bda3be3c3ffec9679de0741
ystk kai <ystk.kai@gmail.com>
2021-08-29 22:50:43 +0900

    Update debian command

--- commands/debian.sh
+++ commands/debian.sh
@@ -4,6 +4,12 @@ apt-get update
 
 apt-get install --no-install-recommends -y \
 curl \
+vim \
 git
 
+# Change the permissions of the mounted private key.
+find ~/.ssh -type f -name "config" -o -name "id_rsa" | xargs -I {} chmod 600 {}
+
+cat << BASHRC >> ~/.bashrc
 export PS1="\n\[\033[32m\]\u@\h \[\033[33m\]\w\[\033[0m\]\n$"
+BASHRC
